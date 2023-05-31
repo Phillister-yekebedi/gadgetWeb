2193b4089f9646ab25614fd6be905d3632087965
Phillister-yekebedi <otienoyekebedi@gmail.com>
2023-05-31 15:47:57 +0300

    first commit

---
+++ Documents/Dom/index.js
@@ -0,0 +1,12 @@
+document.body.style.backgroundColor= 'silver'
+
+document.getElementById('title').style.color = 'green'
+
+let allH3Elements= document.querySelector('h3')
+allH3Elements.style.textTransform = 'uppercase';
+
+
+
+// let tit = document.createElement(''li);
+// h2.innerHTML = "I am a graduate student";
+// document.getElementById('container').appendChild(h2);
