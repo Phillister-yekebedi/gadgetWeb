4f090356d73e09dc504533d9c8b3bd63591c16ae
Phillister-yekebedi <otienoyekebedi@gmail.com>
2023-07-06 15:13:23 +0300

    tweets

---
+++ AndroidStudioProjects/TwitterClone/app/src/main/java/com/example/twitterclone/TweetData.kt
@@ -0,0 +1,13 @@
+package com.example.twitterclone
+
+ data class TweetData {
+    var avatar:String,
+    var displayName:String,
+    var handle:String,
+    var tweet:String
+    var commentCount:Int,
+    var retweetCount:String
+    var likeCount:String
+
+
+}
